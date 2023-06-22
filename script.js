0259ae21fe90e77901acbd80244f6f7814559041
sherzod <sherzodjon010101@gmail.com>
2023-06-22 19:04:06 +0500

    js

---
+++ script.js
@@ -0,0 +1,66 @@
+//var  a = 2
+//console.log(a);
+
+//var b = 3
+//var c = 4
+//var d = b + c
+//console.log(d-b);
+
+
+//var string = "salom"
+//console.log( string);
+
+var ism = "Sherzod"
+var age = 14
+//console.log("Mening ismim " + ism + " va men " + age + " yoshdaman");
+
+
+ var modal = prompt("Ismingizni kiriting")
+console.log("Mening ismim " +modal);
+
+var modal = prompt("yoshingizni kriting")
+console.log("Men " + modal + " yoshdaman");
+
+
+var modal = prompt("5+6")
+console.log("Misol 1: 5 + 6 = " +(5+6)  + " Sizning javob " + +modal )
+
+var modal = prompt("22+33")
+console.log("Misol 2: 22 + 33 = " +(22+33)  + " Sizning javob " + +modal )
+
+var modal = prompt("18-11")
+console.log("Misol 3: 50 - 44 = " +(50-44)  + " Sizning javob " + +modal )
+
+var modal = prompt("50-44")
+console.log("Misol 4: 18 - 11 = " +(18-11)  + " Sizning javob " + +modal )
+
+var modal = prompt("21*2")
+console.log("Misol 5: 21 * 2 = " +(21*2)  + " Sizning javob " + +modal )
+
+var modal = prompt("45+7")
+console.log("Misol 6: 45 + 7 = " +(45+7)  + " Sizning javob " + +modal )
+
+var modal = prompt("30+8")
+console.log("Misol 7: 30 + 8 = " +(30+8)  + " Sizning javob " + +modal )
+
+var modal = prompt("57+6")
+console.log("Misol 8: 57 + 6 = " +(57+6)  + " Sizning javob " + +modal )
+
+var modal = prompt("52+5")
+console.log("Misol 9: 52 + 6 = " +(52+5)  + " Sizning javob " + +modal )
+
+var modal = prompt("49+4")
+console.log("Misol 10: 49 + 4 = " +(49+4)  + " Sizning javob " + +modal )
+
+var modal = prompt("5+6")
+console.log("Misol 11: 56 - 8 = " +(56-8)  + " Sizning javob " + +modal )
+
+var modal = prompt("77-8")
+console.log("Misol 12: 77 - 8 = " +(77-8)  + " Sizning javob " + +modal )
+
+var modal = prompt("53 6")
+console.log("Misol 13: 53 - 6 = " +(53-6)  + " Sizning javob " + +modal )
+
+
+
+
